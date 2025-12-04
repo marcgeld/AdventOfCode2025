ce67d0b720b662ae71bf1315087da3010f528f64
Marcus Gelderman <marcgeld@gmail.com>
2025-12-04 16:49:32 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "AdventOfCode2025",
+    products: [
+        .executable(name: "day01", targets: ["day01"]),
+        .executable(name: "day02", targets: ["day02"]),
+    ],
+    targets: [
+        .target(
+            name: "Shared",
+            path: "Sources/Shared"
+        ),
+        .executableTarget(
+            name: "day01",
+            dependencies: ["Shared"],
+            path: "Sources/day01",
+            resources: [
+                .copy("input.txt")
+            ]
+        ),
+        .executableTarget(
+            name: "day02",
+            dependencies: ["Shared"],
+            path: "Sources/day02",
+            resources: [
+                .copy("input.txt")
+            ]
+        ),
+        .executableTarget(
+            name: "day03",
+            dependencies: ["Shared"],
+            path: "Sources/day03",
+            resources: [
+                .copy("input.txt")
+            ]
+        ),
+        .executableTarget(
+            name: "day04",
+            dependencies: ["Shared"],
+            path: "Sources/day04",
+            resources: [
+                .copy("input.txt")
+            ]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,20 @@
+# AdventOfCode2025 🎄
+
+Solutions to [Advent of Code 2025](https://adventofcode.com/2025/), implemented in Swift.
+
+Each day is a fully standalone executable target.  
+This makes every solution isolated, clean, and easy to run or test independently.
+
+## Build & Run 🚀
+
+Build all targets:
+
+```bash
+swift build
+```
+
+## Run a specific day 🐝
+
+```bash
+swift run day01
+````
